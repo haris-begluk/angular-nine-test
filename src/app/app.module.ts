3b172726915ca4f4501fcbd2f736695742951ee0
Haris-Begluk <haris.begluk@edu.fit.ba>
2020-03-31 16:06:21 +0200

    Cleaning project adding material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
-import { FirstComponent } from './components/home/first/first.component';
+
 import { RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { AppRoutingModule } from './../app-routing/app-routing.module';
-import { AuthorizationModule } from './../authorization/authorization.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, FirstComponent],
-  imports: [BrowserModule, AuthorizationModule, AppRoutingModule, RouterModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/authorization/Models/student.model.ts
+++
@@ -1,7 +0,0 @@
-export class Student {
-    id: Number;
-    name: String;
-    enrollNumber: Number;
-    college: String;
-    university: String;
-}
