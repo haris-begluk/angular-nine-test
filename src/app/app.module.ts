2d822377204a24fa8379cda035487ba21e3a4a0c
Haris-Begluk <haris.begluk@edu.fit.ba>
2020-03-31 16:48:43 +0200

    material navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule, 
+    RouterModule, 
+    BrowserAnimationsModule,
+    MatToolbarModule,MatMenuModule,MatSliderModule,MatButtonModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
